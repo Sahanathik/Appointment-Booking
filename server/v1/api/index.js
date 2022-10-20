1cfe8a774f61db66c30213eff27caff29661eac0
unknown <sahamsg@gmail.com>
2022-10-20 19:26:24 +0530

    changes-index-file

--- server/v1/api/index.js
+++ server/v1/api/index.js
@@ -5,6 +5,10 @@ import User from './User/user.router.js';
 import Admin from './Admin/admin.router.js';
 import Departments from './Departments/departments.router.js';
 import Specialist from './Specialist/specialist.router.js';
+import appSettings from './App_Settings/app.router.js';
+import paymentSettings from './Payment_Settings/payment.router.js';
+import Appointment from './User/user.router.js'
+
 
 app.use('/user', User)
 app.use('/admin', Admin)
